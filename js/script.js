f9a3ac84a5e32f8595592e23861877dea1f3081a
Breno <breno.givas@icloud.com>
2024-01-16 21:11:30 -0300

    feat: responsividade

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 const productsMenu = document.querySelector('.item-1')
-console.log(productsMenu)
+const closeMenu = document.querySelector('.close-icon')
+const openMenu = document.querySelector('.menu-icon')
 const menu = document.querySelector('.menu-hidden')
-console.log(menu)
+const menuMobile = document.querySelector('.menu-hidden-mobile')
 
 productsMenu.addEventListener('click', () => {
   menu.classList.toggle('active')
+})
+
+openMenu.addEventListener('click', () => {
+  menuMobile.classList.toggle('active')
+})
+
+closeMenu.addEventListener('click', () => {
+  menuMobile.classList.toggle('active')
 })
