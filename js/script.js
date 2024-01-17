10a486dec36dd1a6fb8beddd65611502df2912f9
Breno <breno.givas@icloud.com>
2024-01-17 19:47:12 -0300

    feat: menu responsividade

--- js/script.js
+++ js/script.js
@@ -1,17 +1,54 @@
 const productsMenu = document.querySelector('.item-1')
-const closeMenu = document.querySelector('.close-icon')
+const guideMenu = document.querySelector('.close-icon')
+const closeMenu = document.querySelector('#close-icon')
 const openMenu = document.querySelector('.menu-icon')
 const menu = document.querySelector('.menu-hidden')
 const menuMobile = document.querySelector('.menu-hidden-mobile')
+const menuMobileOptions = document.querySelectorAll('.menu-list-mobile')
+const allOptions = document.querySelectorAll('.menu-list-hidden-mobile')
+const subtitle = document.querySelectorAll('.subtitle')
+const title = document.querySelector('.title')
 
 productsMenu.addEventListener('click', () => {
   menu.classList.toggle('active')
 })
 
 openMenu.addEventListener('click', () => {
-  menuMobile.classList.toggle('active')
+  title.innerText = ''
+  menuMobile.classList.remove('disable')
+  menuMobileOptions.forEach((option) => {
+    option.classList.remove('disable')
+  })
 })
 
 closeMenu.addEventListener('click', () => {
-  menuMobile.classList.toggle('active')
+  title.innerText = ''
+  menuMobile.classList.add('disable')
+  menuMobileOptions.forEach((option) => {
+    option.classList.add('disable')
+  })
+  subtitle.forEach((option) => {
+    option.classList.remove('disable-display')
+  })
+  allOptions.forEach((element) => {
+    element.classList.add('disable-display')
+  })
+})
+
+subtitle.forEach((option) => {
+  option.addEventListener('click', () => {
+    option.classList.remove('disable-display')
+    menuMobileOptions.forEach((element) => {
+      if (element !== option.parentNode) {
+        element.classList.toggle('disable')
+      } else {
+        const result = element.querySelector('.menu-list-hidden-mobile')
+        title.innerText = option.innerText.toUpperCase()
+        option.classList.add('disable-display')
+        if (result) {
+          result.classList.remove('disable-display')
+        }
+      }
+    })
+  })
 })
